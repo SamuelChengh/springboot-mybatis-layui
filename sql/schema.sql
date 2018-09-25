69e0038d9973d0b82329bb28a10bc65d5eefedd0
SamuelChengh <458648599@qq.com>
2018-09-25 21:41:21 +0800

    // add

--- sql/schema.sql
+++ sql/schema.sql
@@ -15,6 +15,7 @@ CREATE TABLE `sys_user` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `login_name` (`login_name`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
+INSERT INTO `sys_user`(`id`,`login_name`,`nick_name`,`password`,`salt`,`status`,`remark`,`created_date`,`updated_date`) VALUES (1,'admin','admin','50e6c4e6434d1856a4dca196db398b38','L7yGIl',1,'xxxxxx','2018-09-25 21:24:09',NULL);
 
 /*
 * 角色表
@@ -29,7 +30,7 @@ CREATE TABLE `sys_role` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
-
+INSERT INTO `sys_role`(`id`,`name`,`remark`,`created_date`,`updated_date`) VALUES (1,'系统管理员','管理员','2018-09-25 21:26:07',NULL);
 /*
 * 权限表
 */
@@ -46,6 +47,10 @@ CREATE TABLE `sys_authority` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `auth_url` (`auth_url`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
+INSERT INTO `sys_authority`(`id`,`name`,`auth_url`,`parent`,`display_sort`,`remark`,`created_date`,`updated_date`) VALUES (1,'权限管理','',0,1,'系统权限','2018-09-25 21:30:04','2018-09-25 21:32:07');
+INSERT INTO `sys_authority`(`id`,`name`,`auth_url`,`parent`,`display_sort`,`remark`,`created_date`,`updated_date`) VALUES (2,'菜单管理','/menu/list',1,11,'菜单','2018-09-25 21:32:58',NULL);
+INSERT INTO `sys_authority`(`id`,`name`,`auth_url`,`parent`,`display_sort`,`remark`,`created_date`,`updated_date`) VALUES (3,'角色管理','/role/list',1,12,'角色','2018-09-25 21:34:02',NULL);
+INSERT INTO `sys_authority`(`id`,`name`,`auth_url`,`parent`,`display_sort`,`remark`,`created_date`,`updated_date`) VALUES (4,'账号管理','/user/list',1,13,'账号','2018-09-25 21:34:02',NULL);
 
 /*
 * 角色-权限
@@ -56,6 +61,10 @@ CREATE TABLE `sys_role_authority` (
   `authority_id` INT(11) NOT NULL,
   PRIMARY KEY (`role_id`,`authority_id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
+INSERT  INTO `sys_role_authority`(`role_id`,`authority_id`) VALUES (1,1);
+INSERT  INTO `sys_role_authority`(`role_id`,`authority_id`) VALUES (1,2);
+INSERT  INTO `sys_role_authority`(`role_id`,`authority_id`) VALUES (1,3);
+INSERT  INTO `sys_role_authority`(`role_id`,`authority_id`) VALUES (1,4);
 
 /*
 * 用户-角色
@@ -66,3 +75,4 @@ CREATE TABLE `sys_user_role` (
   `role_id` INT(12) NOT NULL,
   PRIMARY KEY (`user_id`,`role_id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
+INSERT  INTO `sys_user_role`(`user_id`,`role_id`) VALUES (1,1);
