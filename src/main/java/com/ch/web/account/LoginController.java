0bdbb9e7982c5b7c75c955742b331c4ef703d46e
SamuelChengh <458648599@qq.com>
2018-09-26 18:15:30 +0800

    // add

--- src/main/java/com/ch/web/account/LoginController.java
+++ src/main/java/com/ch/web/account/LoginController.java
@@ -20,9 +20,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 public class LoginController {
