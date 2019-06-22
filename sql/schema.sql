1585ab1d8d8170d370511e97f57b5a615160a77c
SamuelChengh <458648599@qq.com>
2019-06-22 13:31:42 +0800

    // add

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,7 @@
-/*
-* 用户表
-*/
+/**
+ * 用户表
+ *
+ */
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -16,9 +17,10 @@ CREATE TABLE `sys_user` (
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
 INSERT INTO `sys_user`(`id`,`login_name`,`nick_name`,`password`,`status`,`remark`,`created_date`,`updated_date`) VALUES (1,'admin','admin','d4dc566564e942188de625e9de521b89',1,'','2018-09-26 15:36:28',NULL);
 
-/*
-* 角色表
-*/
+/**
+ * 角色表
+ *
+ */
 DROP TABLE IF EXISTS `sys_role`;
 CREATE TABLE `sys_role` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -31,9 +33,10 @@ CREATE TABLE `sys_role` (
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
 INSERT INTO `sys_role`(`id`,`name`,`remark`,`created_date`,`updated_date`) VALUES (1,'系统管理员','管理员','2018-09-25 21:26:07',NULL);
 
-/*
-* 权限表
-*/
+/**
+ * 权限表
+ *
+ */
 DROP TABLE IF EXISTS `sys_authority`;
 CREATE TABLE `sys_authority` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -54,9 +57,10 @@ INSERT INTO `sys_authority`(`id`,`name`,`auth_url`,`parent`,`display_sort`,`disp
 INSERT INTO `sys_authority`(`id`,`name`,`auth_url`,`parent`,`display_sort`,`display_type`,`icon`,`remark`,`created_date`,`updated_date`) VALUES (3,'角色管理','/role/list',1,12,2,'','角色','2018-09-25 21:34:02',NULL);
 INSERT INTO `sys_authority`(`id`,`name`,`auth_url`,`parent`,`display_sort`,`display_type`,`icon`,`remark`,`created_date`,`updated_date`) VALUES (4,'账号管理','/user/list',1,13,2,'','账号','2018-09-25 21:34:02',NULL);
 
-/*
-* 角色-权限
-*/
+/**
+ * 角色-权限
+ *
+ */
 DROP TABLE IF EXISTS `sys_role_authority`;
 CREATE TABLE `sys_role_authority` (
   `role_id` INT(11) NOT NULL,
@@ -68,9 +72,10 @@ INSERT INTO `sys_role_authority`(`role_id`,`authority_id`) VALUES (1,2);
 INSERT INTO `sys_role_authority`(`role_id`,`authority_id`) VALUES (1,3);
 INSERT INTO `sys_role_authority`(`role_id`,`authority_id`) VALUES (1,4);
 
-/*
-* 用户-角色
-*/
+/**
+ * 用户-角色
+ *
+ */
 DROP TABLE IF EXISTS `sys_user_role`;
 CREATE TABLE `sys_user_role` (
   `user_id` INT(12) NOT NULL,
